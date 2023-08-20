2213b715cf16da2eba1d9a6bc12fec0c801fa8b8
Soumyakanti Ghosh <soumyaghosh.ghosh@gmail.com>
2023-08-20 11:46:41 +0530

    Test microservice with Eureka client

---
+++ src/main/java/com/soumya/demo/eureka/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.soumya.demo.eureka.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping("/hello")
+	public String getResponse(){
+		return "Hello World";
+	}
+
+}
